65eb692339164b32d5b2aeaf19b72139b879588d
[name] <prashant.patchi@gmail.com>
2018-12-27 00:44:34 +0100

    Initial commit

---
+++ app/src/main/java/win/prashant/tabfragment/frgmentpageadapter.java
@@ -0,0 +1,40 @@
+package win.prashant.tabfragment;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class frgmentpageadapter extends FragmentPagerAdapter {
+
+    ArrayList<Fragment>listfregment = new ArrayList<>();
+
+    //title will be stored in list array
+    ArrayList<String>listtitle =new ArrayList<>();
+
+    public frgmentpageadapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return listfregment.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return listfregment.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return listtitle.get(position);
+    }
+    public void AddFragment(Fragment fragment,String Title){
+        listfregment.add(fragment);
+        listtitle.add(Title);
+    }
+}
